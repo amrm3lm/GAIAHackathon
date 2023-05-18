24833f126715d65c376ac521f2e8ab3591e958af
Amr <amralmoallim@gmail.com>
2023-05-18 16:42:24 -0700

    basic get handler and requirements

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World! - SummarizeX</p>"
+
+@app.route("/summarize")
+def summarize():
+    url = request.args.get('url')
+    res = {}
+    res['url'] = url
+    res['response'] = "very very good product!"
+    return res
