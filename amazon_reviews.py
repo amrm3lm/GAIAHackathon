b16fc8e494c9a8215b719b80cb920b4d2491da48
Amr <amralmoallim@gmail.com>
2023-05-19 10:54:50 -0700

    added api calls

--- amazon_reviews.py
+++ amazon_reviews.py
@@ -1,7 +1,25 @@
 from amazon_reviews_scrapper import amazon_product_review_scraper
+import requests
+import json
+
 print("hello world")
 
-review_scraper = amazon_product_review_scraper(amazon_site="amazon.com", product_asin="B07X6V2FR3")
-reviews_df = review_scraper.scrape()
-#print(reviews_df)
-print(reviews_df.head(5))
+# review_scraper = amazon_product_review_scraper(amazon_site="amazon.com", product_asin="B07X6V2FR3")
+# reviews_df = review_scraper.scrape()
+# #print(reviews_df)
+# print(reviews_df.head(5))
+
+# set up the request parameters
+params = {
+'api_key': '6642D72A9FBD4911B063B399E3C2939C',
+  'amazon_domain': 'amazon.com',
+  'asin': 'B0C2VND51M',
+  'type': 'product',
+  'output': 'json',
+  'include_summarization_attributes': 'true'
+}
+
+# make the http GET request to ASIN Data API
+api_result = requests.get('https://api.asindataapi.com/request', params)
+
+print(api_result)
--- main.py
+++ main.py
@@ -1,15 +1,157 @@
 from flask import Flask, request
 
 app = Flask(__name__)
+import ssl
+context = ssl.SSLContext()
+context.load_cert_chain('cert.pem', 'key.pem')
+from urllib.parse import urlparse
+import re
+import requests
+from settings import api_keys
 
+import cohere
+
+
+asin_reg = "(?:[/dp/]|$)([A-Z0-9]{10})"
+REVIEWS_MAX_PAGES = 5
 @app.route("/")
 def hello_world():
     return "<p>Hello, World! - SummarizeX</p>"
 
 @app.route("/summarize", methods = ['POST'])
 def summarize():
-    url = request.form.get('url')
-    res = {}
+    url = request.json['url']
+    res = summarize_handler(request.json)
     res['url'] = url
-    res['response'] = "very very good product!"
+
     return res
+
+@app.route("/generative_summary", methods = ['POST'])
+def generative_summary():
+    url = request.json['url']
+    res = generate_summary_handler(request.json)
+    res['url'] = url
+
+    return res
+
+def get_domain_and_asin(url, res):
+    #check if its coming from amazon
+    domain = urlparse(url).netloc
+    if "amazon" not in domain:
+        res['error'] = 400
+        res['error_msg'] = "not amazon domain"
+        return res
+
+    first = domain.find("amazon")
+    domain = domain[first:]
+    #extracting asin
+    #https://www.amazon.com/Lasko-U35115-Electric-Oscillating-Velocity/dp/B081HDGZML?ref_=Oct_DLandingS_D_e95f1a2b_2&th=1
+    #m = re.match(asin_reg, url)
+    m = re.search(r'/[dg]p/([^/]+)', url, flags=re.IGNORECASE)
+    word = m.group(1)
+    if word is not None:
+        if word[0]== '/':
+            asin = word[1:11]
+        else:
+            asin = word[:10]
+    else:
+        res['error'] = 404
+        res['error_msg'] = "ASIN could not be extracted"
+        return res
+
+    print(f"found asin {asin} and domain {domain}")
+    res['asin'] = asin
+    res['domain'] = domain
+
+def generate_summary_handler(request):
+    url = request['url']
+    res = {}
+    get_domain_and_asin(url, res)
+
+    if 'error' in res.keys():
+        return res
+
+    domain = res['domain']
+    asin = res['asin']
+
+    if '.sa' in res['domain']:
+        reviews = reviews_api_wrapper(domain, asin, {'language': 'ar_SA'})
+    else :
+        reviews = reviews_api_wrapper(domain, asin)
+        print("REVIEWS ------------------ ")
+        print(reviews)
+        res['generative'] = run_cohere_generative_summary(reviews)
+
+    return res
+
+
+def summarize_handler(request) :
+    url = request['url']
+    res = {}
+    get_domain_and_asin(url, res)
+
+    if 'error' in res.keys():
+        return res
+
+    domain = res['domain']
+    asin = res['asin']
+
+    #call reviews api
+    if '.sa' in domain:
+        reviews = reviews_api_wrapper(domain, asin, {'language': 'ar_SA'})
+    else :
+        reviews = reviews_api_wrapper(domain, asin)
+        res['summary'] = run_cohere_summarization(reviews)
+
+    return res
+
+def run_arabic_summarization(reviews) :
+    API_URL = "https://api-inference.huggingface.co/models/malmarjeh/t5-arabic-text-summarization"
+    headers = {"Authorization": f"Bearer {api_keys['huggingface_arabic_bearer']}"}
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+def run_cohere_summarization(reviews) :
+    client = cohere.Client(api_keys['cohere'])
+    text = "\n".join(reviews)
+    summary = client.summarize(text)
+    return summary.summary
+
+def run_cohere_generative_summary(reviews) :
+    text = "\n".join(reviews)
+
+    prompt = f"Each new line contains a product review from a customer. At the end a summary of the overall sentiment towards the product, the main advantages and disadvantages of the product, the main qualitative descriptors used for the product, will be written:" \
+             f"{text}  " \
+             f" In summary: "
+    client = cohere.Client(api_keys['cohere'])
+    summary = client.generate(prompt)
+    return summary.generations[-1].text
+
+def reviews_api_wrapper(domain, asin, options={}) :
+    params = {
+        'api_key': api_keys['amazon_reviews'],
+        'amazon_domain': domain,
+        'asin': asin,
+        'type': 'reviews',
+        'output': 'json',
+        'max_page': REVIEWS_MAX_PAGES,
+        **options
+    }
+    print("params = ", params)
+
+    # make the http GET request to ASIN Data API
+    api_result = requests.get('https://api.asindataapi.com/request', params)
+    res_json = api_result.json()
+    #extract reviews only
+    reviews = [x['body'] for x in res_json['reviews']]
+    return reviews
+
+
+def test_sum():
+    url = 'https://www.amazon.com/Lasko-U35115-Electric-Oscillating-Velocity/dp/B081HDGZML?ref_=Oct_DLandingS_D_e95f1a2b_2&th=1'
+    res = summarize_handler(url)
+    print(res)
+
+if __name__ == "__main__":
+    #app.run(ssl_context=context)
+    test_sum()
